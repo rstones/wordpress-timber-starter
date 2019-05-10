2f1511ece05b0625705dc8483c5552a474209982
Richard Stones <richardstones10@gmail.com>
2019-05-10 08:35:24 +0100

    Configured webpack to build js and scss files and include in theme

--- wordpress/wp-content/themes/timber-starter-theme/functions.php
+++ wordpress/wp-content/themes/timber-starter-theme/functions.php
@@ -141,4 +141,7 @@ class StarterSite extends Timber\Site {
 
 }
 
+wp_enqueue_style('site_main_css', get_template_directory_uri() . '/css/build/style.css');
+wp_enqueue_script('site_main_js', get_template_directory_uri() . '/js/build/bundle.js', null, null, true);
+
 new StarterSite();
---
+++ wordpress/wp-content/themes/timber-starter-theme/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+// include js minification plugin
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+// include css extraction and minification plugins
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    entry: ['./js/src/main.js', './css/src/main.scss'],
+    output: {
+        filename: './js/build/bundle.js',
+        path: path.resolve(__dirname)
+    },
+    module: {
+        rules: [
+            // perform babelization on all js files
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
+                }
+            },
+            // compile scss files to plain css
+            {
+                test: /\.(sass|scss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        //extract css into dedicated file
+        new MiniCssExtractPlugin({
+            filename: './css/build/style.css' 
+        })
+    ],
+    optimization: {
+        minimizer: [
+            // enable js minification plugin
+            new UglifyJSPlugin({
+                cache: true,
+                parallel: true
+            }),
+            // enable css minification plugin
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    }
+};
