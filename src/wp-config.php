195e378367ec1574aefa64c281abd781fbca37e9
Richard Stones <richardstones10@gmail.com>
2019-05-17 10:11:13 +0100

    defined constant to allow filesystem writing for plugin installation via admin site

--- src/wp-config.php
+++ src/wp-config.php
@@ -88,3 +88,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+
+/** Allows write access to filesystem for installing plugins */
+define('FS_METHOD', 'direct');
